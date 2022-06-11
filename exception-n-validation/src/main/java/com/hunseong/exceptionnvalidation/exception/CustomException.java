d70cc73dcf76ce3404e728f00f08b72efaed7481
Hunseong <phunseong@gmail.com>
2022-06-11 16:10:36 +0900

    add: CustomException extends RuntimeException

---
+++ exception-n-validation/src/main/java/com/hunseong/exceptionnvalidation/exception/CustomException.java
@@ -0,0 +1,22 @@
+package com.hunseong.exceptionnvalidation.exception;
+
+import lombok.Getter;
+
+/**
+ * Created by Hunseong on 2022/06/11
+ */
+@Getter
+public class CustomException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public CustomException(ErrorCode errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+}
