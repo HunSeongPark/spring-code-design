3445e8ec63c3124fbe9ec5cb2e234670e0e9f75a
Hunseong <phunseong@gmail.com>
2022-06-12 02:07:40 +0900

    initial commit

---
+++ event-publisher/src/main/java/com/hunseong/eventpublisher/EventPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.hunseong.eventpublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventPublisherApplication.class, args);
+	}
+
+}
