831e3dc274276cf108a5fe5619c5ef83e9e537a2
Hunseong <phunseong@gmail.com>
2022-06-11 16:04:48 +0900

    initial commit

---
+++ exception-n-validation/src/main/java/com/hunseong/exceptionnvalidation/ExceptionNValidationApplication.java
@@ -0,0 +1,13 @@
+package com.hunseong.exceptionnvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionNValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionNValidationApplication.class, args);
+	}
+
+}
---
+++ exception-n-validation/src/test/java/com/hunseong/exceptionnvalidation/ExceptionNValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hunseong.exceptionnvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionNValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
